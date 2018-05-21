10f106d85559c715e78015773a58b0baa5f9aade
Siddhu Nallasivam <kousiksiddhu@gmail.com>
2018-05-21 22:56:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 import { RouteRoutingModule } from './route/route-routing.module';
 import { RouteModule } from './route/route.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageZoomModule } from 'angular2-image-zoom';
+
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -12,7 +15,6 @@ import { AboutFifaComponent } from './about-fifa/about-fifa.component';
 import { DevelopmentComponent } from './development/development.component';
 import { GovernanceComponent } from './governance/governance.component';
 import { SustainabilityComponent } from './sustainability/sustainability.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,8 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awes
     RouteModule,
     RouteRoutingModule,
     BrowserAnimationsModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    ImageZoomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -11,5 +11,13 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit() {
   }
+  zoomedImageSrc = "http://shoptommy.scene7.com/is/image/ShopTommy/78A2884_083_BCK";
+  smallImageSrc = "http://shoptommy.scene7.com/is/image/ShopTommy/78A2884_083_FNT";
+  width = "300";
+  height = "400";
+  
 
+  //
+  //<img [imageZoom]="zoomedImageSrc" [src]="smallImageSrc" [style.width.px]="width" [style.height.px]="height">
+  
 }
